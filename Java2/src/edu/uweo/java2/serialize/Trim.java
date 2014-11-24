b00a01c167fd489e7c488f13baf5c6cf422590dc
James Gleixner <James.Gleixner@expeditors.com>
2014-11-24 15:48:35 -0800

    serialize homework

---
+++ Java2/src/edu/uweo/java2/serialize/Trim.java
@@ -0,0 +1,16 @@
+package edu.uweo.java2.serialize;
+
+import edu.uweo.java2.homework.serialize.SerializableObject;
+
+public class Trim {
+
+	public Trim() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
