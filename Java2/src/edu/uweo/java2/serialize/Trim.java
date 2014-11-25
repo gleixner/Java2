218f9ff800c409d1f375a12577fe2b2353c2b7e1
James Gleixner <James.Gleixner@expeditors.com>
2014-11-24 16:30:33 -0800

    more work

--- Java2/src/edu/uweo/java2/serialize/Trim.java
+++ Java2/src/edu/uweo/java2/serialize/Trim.java
@@ -2,18 +2,18 @@ package edu.uweo.java2.serialize;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import serialization.Employee;
 import edu.uweo.java2.homework.serialize.SerializableObject;
 
 public class Trim {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		String input = "C:\\Users\\chq-jamesgl\\git\\Java2\\Java2\\resources\\to_trim.ser";
 		String output = "C:\\Users\\chq-jamesgl\\git\\Java2\\Java2\\resources\\to_trimmed.ser";
 		Trim trm = new Trim( input, output );
@@ -34,23 +34,23 @@ public class Trim {
 		outputList = new ArrayList<>();
 	}
 
-	public void process() {
+	public void process() throws IOException, ClassNotFoundException {
 		deserializeObjects();
 		trim();
 		serializeObject();
 	}
 
-	private void deserializeObjects() {
+	private void deserializeObjects() throws IOException, ClassNotFoundException {
 		try(ObjectInputStream in = new ObjectInputStream( new FileInputStream( input )) ) {
 			list  = ( List<SerializableObject> ) in.readObject();
 			toTrim = ( List<Integer> ) in.readObject();
 		}
 	}
 
-	private void serializeObject() {
+	private void serializeObject() throws IOException, ClassNotFoundException {
 		try( ObjectOutputStream out = 
 				new ObjectOutputStream( new FileOutputStream( output ) ) ) {
-			
+			out.writeObject( outputList );
 		}
 	}
 
