5c92bd5fa23e0317fdf92828633a823ac32d46ec
James Gleixner <James.Gleixner@expeditors.com>
2014-12-01 16:26:07 -0800

    Building up test

--- Java2/src/edu/uweo/java2/homework/threads/TaskThreaderTest.java
+++ Java2/src/edu/uweo/java2/homework/threads/TaskThreaderTest.java
@@ -6,6 +6,9 @@ public class TaskThreaderTest {
 		int ThreadCount = 10;
 		TaskThreader threader = new TaskThreader( ThreadCount );
 		threader.Process();
+		Thread[] threadList = threader.getThreads();
+		Thread interrupter = threadList[ ThreadCount - 2 ];
+		interrupter.interrupt();
 		try {
 			Thread.sleep(100);
 		} catch (InterruptedException e) {
